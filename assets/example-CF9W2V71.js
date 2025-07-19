957d5839c8d94a48a48eebcc852f492ce3208cfe
pabloaaf <pabloaaf@users.noreply.github.com>
2025-07-19 14:00:20 +0000

    deploy: 0a85d8cf9a13965599b92d0c1fde1681f142f2be

---
+++ assets/example-CF9W2V71.js
@@ -0,0 +1,3 @@
+import{j as e,C as o,H as r}from"./index-eFzkZ-yE.js";const a={id:"ejemplo",title:"Un ejemplo de MDX",date:"2025-07-13"};function s(n){const t={h2:"h2",p:"p",...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h2,{children:"Este es un post"}),`
+`,e.jsx("br",{}),`
+`,e.jsxs(t.p,{children:["Puedes usar ",e.jsx(o,{children:"componentes"})," dentro de tu ",e.jsx(r,{children:"contenido"})," gracias a MDX."]})]})}function i(n={}){const{wrapper:t}=n.components||{};return t?e.jsx(t,{...n,children:e.jsx(s,{...n})}):s(n)}export{i as default,a as frontmatter};
