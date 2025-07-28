b14b9b8567a43f906971308791b7a7898e6c54d7
pabloaaf <pabloaaf@users.noreply.github.com>
2025-07-28 22:03:59 +0000

    deploy: 26a8cae63164fa290ba1f9aa70a9e8ebef5a0400

--- assets/example-B8-CrnNg.js
+++ assets/example-B8-CrnNg.js
@@ -1,3 +1,3 @@
-import{j as e,C as s,H as r}from"./index-CIRkbSqS.js";const i={id:"ejemplo",title:"Un ejemplo de MDX",date:"2025-07-13",description:"resumen corto"};function o(n){const t={h2:"h2",p:"p",...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h2,{children:"Este es un post"}),`
+import{j as e,C as s,H as r}from"./index-DVhwk8V8.js";const i={id:"ejemplo",title:"Un ejemplo de MDX",date:"2025-07-13",description:"resumen corto"};function o(n){const t={h2:"h2",p:"p",...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h2,{children:"Este es un post"}),`
 `,e.jsx("br",{}),`
 `,e.jsxs(t.p,{children:["Puedes usar ",e.jsx(s,{children:"componentes"})," dentro de tu ",e.jsx(r,{children:"contenido"})," gracias a MDX."]})]})}function a(n={}){const{wrapper:t}=n.components||{};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{a as default,i as frontmatter};
